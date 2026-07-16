8e9feafa718fcbce7d3ee1e2d08eb242f53a97fa
talpateshubham2000 <talpateshubham2000@gmail.com>
2026-07-16 18:49:14 +0530

    Create HealthController.java
    adding helth controller

---
+++ backend/face-attendance-backend/src/main/java/com/attendance/facesystem/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.attendance.facesystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend Running";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
